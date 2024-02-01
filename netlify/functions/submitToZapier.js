5c0fb5336dc1fb4f9ac5b86f8bd68a56bd903405
niftyhoops <tomm.becker@niftyhoops.com>
2024-02-01 11:32:23 -0500

    inserted all valueMappings

--- netlify/functions/submitToZapier.js
+++ netlify/functions/submitToZapier.js
@@ -93,12 +93,6 @@ const optionMappings = {
     "v_647489_ldnakpep": "101-200 miles",
     // Continue for each value...
 
-    // Kit Options for different tiers
-    "v_6472341": "Double-layer plastic with low voltage inflation",
-    "v_6472342": "Louvers",
-    "v_6472343": "Louver automation (motors, conduit, and thermostat control)",
-    // Continue for each value...
-
     // Delivery & Installation options
     "v_643521_lddl6lik": "Pickup from Nifty Hoops (48103) - no charge",
     "v_4030629": "Delivery - quoted separately",
@@ -158,9 +152,124 @@ const optionMappings = {
 
 // Simplified for demonstration
 const valueMappings = {
-    'v_4030587': "Manual 5:1 Hand Cranks",
-    'v_4030588': "Motorized and Thermostatically Controlled",
-    // Continue for each value...
+    // 30' Gothic Options Lengths
+
+    "v_4029679": "30x48 Gothic Hoophouse",
+    "v_4029680": "30x72 Gothic Hoophouse",
+    "v_4029681": "30x96 Gothic Hoophouse",
+    "v_4029682": "30x120 Gothic Hoophouse",
+    "v_643257_ldd2dzk7": "30x144 Gothic Hoophouse",
+    "v_643257_ldd2e6xf": "30x168 Gothic Hoophouse",
+    "v_643257_ldd2etqj": "30x192 Gothic Hoophouse",
+
+    // 22.5' Gothic Options Lengths
+    "v_4043553": "22.5x24 Gothic Hoophouse",
+    "v_4043554": "22.5x36 Gothic Hoophouse",
+    "v_4043555": "22.5x48 Gothic Hoophouse",
+    "v_4043556": "22.5x72 Gothic Hoophouse",
+    "v_4043557": "22.5x96 Gothic Hoophouse",
+
+    // 16' Options Lengths
+    "v_6472337": "16x16 Gothic Hoophouse",
+    "v_6472338": "16x24 Gothic Hoophouse",
+    "v_6472339": "16x32 Gothic Hoophouse",
+    "v_1025051_iwsgajoxvjksclbosp": "16x40 Gothic Hoophouse",
+    "v_1025051_1ux1kjnzsjwspd7zhq": "16x48 Gothic Hoophouse",
+
+    // Roll-up Side Operation Options
+    "v_4030587": "Manual 5:1 Hand Cranks",
+    "v_4030588": "Motorized and Thermostatically Controlled",
+    "v_4043570": "Manual 5:1 Hand Cranks",
+    "v_4043571": "Motorized and Thermostatically Controlled",
+
+    // Overhead Plastic Options
+"v_4029697": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (48')",
+"v_4029698": "Single-layer 6-mil, 4-year poly (48')",
+"v_643265_lddjtssb": "No overhead plastic (48')",
+"v_4030578": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (72')",
+"v_4030579": "Single-layer 6-mil, 4-year poly (72')",
+"v_643504_lddjz8f7": "No Overhead Plastic (72')",
+"v_4030580": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (96')",
+"v_4030581": "Single-layer 6-mil, 4-year poly (96')",
+"v_643505_lddjzynm": "No Overhead Plastic (96')",
+"v_4030582": "Double-layer (Clear & IRAC) 6-mil, 4 year & 12V inflation (120')",
+"v_4030583": "Single-layer 6-mil, 4-year poly (120')",
+"v_643506_lddk0sr9": "No Overhead Plastic (120')",
+"v_4071322": "No overhead plastic (144')",
+"v_4071323": "Single-layer 6-mil, 4-year poly (144')",
+"v_4071324": "Double-layer (Clear & IRAC) 6-mil, 4 year & 12V inflation (144')",
+"v_4071325": "No overhead plastic (168')",
+"v_4071326": "Single-layer 6-mil, 4-year poly (168')",
+"v_4071327": "Double-layer (Clear & IRAC) 6-mil, 4 year & 12V inflation (168')",
+"v_4071328": "No overhead plastic (192')",
+"v_4071329": "Single-layer 6-mil, 4-year poly (192')",
+"v_4071330": "Double-layer (Clear & IRAC) 6-mil, 4 year & 12V inflation (192')",
+"v_4043601": "No overhead plastic (24'-long)",
+"v_4043602": "Single-layer 6-mil, 4-year Poly (24'-long)",
+"v_4043603": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (24'-long)",
+"v_4043604": "No overhead plastic (36'-long)",
+"v_4043605": "Single-layer 6-mil, 4-year Poly (36'-long)",
+"v_4043606": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (36'-long)",
+"v_4043607": "No overhead plastic (48'-long)",
+"v_4043608": "Single-layer 6-mil, 4-year Poly (48'-long)",
+"v_4043609": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (48'-long)",
+"v_4043610": "No overhead plastic (72'-long)",
+"v_4043611": "Single-layer 6-mil, 4-year Poly (72'-long)",
+"v_4043612": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (72'-long)",
+"v_4043613": "No overhead plastic (96'-long)",
+"v_4043614": "Single-layer 6-mil, 4-year Poly (96'-long)",
+"v_4043615": "Double-layer (Clear over IRAC) 6-mil, 4 year & 12V inflation (96'-long)",
+
+     // Delivery & Installation Options
+
+
+"v_643521_lddl6lik": "Pickup from Nifty Hoops (48103) - no charge",
+"v_4030629": "Delivery - quoted separately",
+"v_4030630": "Starter Build - quoted separately",
+"v_4030631": "Community Build - quoted separately",
+"v_4030632": "Full Installation - quoted separately",
+"v_4043680": "Pickup from Nifty Hoops (48103) - no charge",
+"v_4043681": "Delivery - quoted separately",
+"v_4043682": "Starter Build - quoted separately",
+"v_4043683": "Community Build - quoted separately",
+"v_4043684": "Full Installation - quoted separately",
+
+     // Kit Options for 16
+
+"v_6472341": "Double-layer plastic with low voltage inflation",
+"v_6472342": "Louvers",
+"v_6472343": "Louver automation (motors, conduit, and thermostat control)",
+"v_1025053_tnmlb1zsyzmbulh4h2": "Side curtain automation (motors and thermostatic control)",
+"v_8593799": "Double-layer plastic with low voltage inflation",
+"v_8593800": "Louvers",
+"v_8593801": "Louver automation (motors, conduit, and thermostat control)",
+"v_8593802": "Side curtain automation (motors and thermostatic control)",
+"v_8593803": "Double-layer plastic with low voltage inflation",
+"v_8593804": "Louvers",
+"v_8593805": "Louver automation (motors, conduit, and thermostat control)",
+"v_8593806": "Side curtain automation (motors and thermostatic control)",
+"v_8593807": "Double-layer plastic with low voltage inflation",
+"v_8593808": "Louvers",
+"v_8593809": "Louver automation (motors, conduit, and thermostat control)",
+"v_8593810": "Side curtain automation (motors and thermostatic control)",
+"v_8593811": "Double-layer plastic with low voltage inflation",
+"v_8593812": "Louvers",
+"v_8593813": "Louver automation (motors, conduit, and thermostat control)",
+"v_8593814": "Side curtain automation (motors and thermostatic control)",
+
+
+     // Endwall ventilation options
+
+"v_4030599": "No Louver Vents",
+"v_4030598": "Automatic 4'W x 3'H Vents (12V actuators with thermostatic control)",
+"v_4043678": "No Louver Vents",
+"v_4043679": "Automatic 4'W x 2'H Vents (12V actuators with thermostatic control)",
+
+     // Conduit options
+
+"v_4302593": "No conduit",
+"v_4302594": "PVC conduit and boxes",
+
 };
 
 exports.handler = async function(event, context) {
