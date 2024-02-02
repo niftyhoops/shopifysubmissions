964f4e3358753cd060fadb27f45fe8ae8c3d15ea
niftyhoops <tomm.becker@niftyhoops.com>
2024-02-02 07:44:10 -0500

    comment out fetch

--- netlify/functions/submitToZapier.js
+++ netlify/functions/submitToZapier.js
@@ -1,4 +1,4 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 // Example mapping from the provided JSON
 const optionMappings = {
