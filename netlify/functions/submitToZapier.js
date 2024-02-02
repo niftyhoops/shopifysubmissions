954c0e082fab4e3b27af61438b8c14ab79bf665d
niftyhoops <tomm.becker@niftyhoops.com>
2024-02-02 07:26:04 -0500

    changing exports.handler

--- netlify/functions/submitToZapier.js
+++ netlify/functions/submitToZapier.js
@@ -349,49 +349,4 @@ exports.handler = async function(event, context) {
 
 
 
-            // Log the original and mapped configurations here, after they are defined
-            console.log("Original productConfig:", productConfig);
-            console.log("Mapped productConfig:", mappedConfig);
 
-            // Update the body with the mapped configuration
-            body.productConfig = mappedConfig;
-
-            const response = await fetch('https://hooks.zapier.com/hooks/catch/6939704/3qzeaip/', {
-                method: 'POST',
-                body: JSON.stringify(body),
-                headers: { 'Content-Type': 'application/json' },
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            return {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ message: "Data processed" })
-            };
-        } catch (error) {
-            return {
-                statusCode: 500,
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ message: error.message })
-            };
-        }
-    }
-
-    // If not OPTIONS or POST
-    return {
-        statusCode: 405, // Method Not Allowed
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        },
-        body: JSON.stringify({message: "Method not allowed"})
-    };
-};
